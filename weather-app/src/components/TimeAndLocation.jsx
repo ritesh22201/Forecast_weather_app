714ecd56e61f13039363adfe144109db5b7e1e6e
ritesh22201 <riteshgoswami22201@gmail.com>
2023-07-04 19:06:23 +0530

    code added

---
+++ weather-app/src/components/TimeAndLocation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+        <div className='flex items-center justify-center my-6'>
+            <p className='text-white text-xl font-extralight'>
+                Tuesday, 4 July 2023 | Local time: 19:05
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation;
